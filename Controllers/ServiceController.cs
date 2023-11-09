e1b8dee291a44582568e9179d6d5eaf0e5ecf9c6
YourSquip <86711878+YourSquip@users.noreply.github.com>
2023-11-10 02:58:57 +0300

    Update ServiceController.cs

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -14,7 +14,7 @@ using System.Reflection.PortableExecutable;
 
 //So the main idea was to form a POST request to dadata get that json and return it in GET request
 //But I wasn't able to send correct POST request to dadata (for some reason)(actually that was 400 failed to read request)
-//But I setted up everything else so you can look up at it ceparately through POST and GET requests in OPENApi
+//But I setted up everything else so you can look up at it separately through POST and GET requests in OPENApi
 //
 namespace StandardizeAddressWebAPI.Controllers
 {
